eb11ad42b86df66aca40e5f5f150df6cac8a1889
Timur-Muradov <timur.muradov.1996@mail.ru>
2023-05-29 21:31:21 +0300

    Здравствуй, дорогой ревьюер) в этом коммите я переверстал экран ProfileViewController кодом, добавил кнопку share на SingleImageViewController и добавил slrollView

--- ImageFeed/ProfileViewController/ProfileViewController.swift
+++ ImageFeed/ProfileViewController/ProfileViewController.swift
@@ -9,21 +9,105 @@ import Foundation
 import UIKit
 
 class ProfileViewController: UIViewController {
-     
-    @IBOutlet private var avatarImageView: UIImageView!
-    @IBOutlet private var nameLabel: UILabel!
-    @IBOutlet private var loginNameLabel: UILabel!
-    @IBOutlet private var descriptionLabel: UILabel!
-
-    @IBOutlet private var logoutButton: UIButton!
-    @IBAction private func didTapLogoutButton() {
-    }
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let imageView = photofunc()
+        let labelName = namefunc()
+        let nickName = nNamefunc()
+        let discription = discfunc()
+        let button = escButton()
+        
+        
+        NSLayoutConstraint.activate([
+            
+            imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            imageView.widthAnchor.constraint(equalToConstant: 70),
+            imageView.heightAnchor.constraint(equalToConstant: 70),
+            
+            
+            labelName.leadingAnchor.constraint(equalTo: imageView.leadingAnchor),
+            labelName.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8),
+            
+            nickName.leadingAnchor.constraint(equalTo: labelName.leadingAnchor),
+            nickName.topAnchor.constraint(equalTo: labelName.bottomAnchor, constant: 8),
+            
+            discription.leadingAnchor.constraint(equalTo: nickName.leadingAnchor),
+            discription.topAnchor.constraint(equalTo: nickName.bottomAnchor, constant: 8),
+            
+            button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            button.centerYAnchor.constraint(equalTo: imageView.centerYAnchor)
+            
+        ])
+        
     }
     
+    func photofunc() -> UIImageView {
+        
+        let profileImage = UIImage(named: "Photo")
+        let imageView = UIImageView(image: profileImage)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+        return imageView
+        
+    }
+    
+    func namefunc() -> UILabel {
+        
+        let labelName = UILabel()
+        labelName.text = "Екатерина Новикова"
+        labelName.textColor = .ypWhite
+        labelName.font = UIFont.systemFont(ofSize: 23)
+        view.addSubview(labelName)
+        labelName.translatesAutoresizingMaskIntoConstraints = false
+        return labelName
+        
+    }
+    
+    func nNamefunc() -> UILabel {
+        
+        let nickName = UILabel()
+        nickName.text = "@ekaterina_nov"
+        nickName.textColor = .ypGray
+        nickName.font = UIFont.systemFont(ofSize: 13)
+        view.addSubview(nickName)
+        nickName.translatesAutoresizingMaskIntoConstraints = false
+        return nickName
+        
+    }
+    
+    func discfunc() -> UILabel {
+        
+        let discription = UILabel()
+        discription.text = "Hello World"
+        discription.textColor = .ypWhite
+        discription.font = UIFont.systemFont(ofSize: 13)
+        view.addSubview(discription)
+        discription.translatesAutoresizingMaskIntoConstraints = false
+        return discription
+        
+    }
+    
+    func escButton() -> UIButton {
+        
+        let button = UIButton.systemButton(
+            with: UIImage(systemName: "ipad.and.arrow.forward")!,
+            target: self,
+            action: #selector(Self.didTapButton)
+        )
+        button.tintColor = .red
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        return button
+        
+    }
+    
+    @objc
+    private func didTapButton() {
+        print("Здравствуй, многоуважаемый ревьюер :)")
+        
+    }
 }
